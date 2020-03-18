50f36f629a28fadc6c6e3161fd9d7264a83d020e
Ghidon Fadlun <ghidon@hotmail.com>
2020-03-18 18:27:02 +0200

    Milestone 2 updated with graph (Graph is showing only last 20 results)

--- companies_script.js
+++ companies_script.js
@@ -1,15 +1,23 @@
+
 // On page load calls function to get symbol parameter from url
 document.addEventListener("DOMContentLoaded", getSymbol)
 
+// Get symbol parameter from url
+// builds url to use with fetch function
+// Calls fetch function
 function getSymbol() {
     let urlParams = new URLSearchParams(window.location.search);
     symbol = urlParams.get('symbol')
+    company = symbol
     data_source = "https://financialmodelingprep.com/api/v3/company/profile/" + symbol
-    console.log(data_source)
-    fetch_data(data_source)
+    fetchUserSearch(data_source)
+    fetchNewUrl(symbol);
 }
 
-function fetch_data(url) {
+
+// fetch function to get company details
+// DOM manipulation to fill HTML elements with required company details.
+function fetchUserSearch(url) {
     fetch(url)
         .then(response => response.json())
         .then(function (data) {
@@ -29,7 +37,6 @@ function fetch_data(url) {
             let changes_percentage = document.getElementById("changes%")
             changes_percentage.innerText = details.changesPercentage
             let changes = details.changes
-            console.log(changes)
             if (changes < 0) {
                 changes_percentage.classList.add("green_light");
             } else {
@@ -37,3 +44,78 @@ function fetch_data(url) {
             }
         })
 }
+
+
+//fetch function to get data for graph
+//Get array's closure values and date values
+//Slice arrays to get only last 20 results.
+//call function to update the graph
+function fetchNewUrl() {
+    let new_url = `https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}?serietype=line`;
+    fetch(new_url)
+        .then(response => response.json())
+        .then(data => {
+            let historical = data.historical;
+            let dates = historical.map(company => company.date);
+            xData = dates.slice(Math.max(dates.length - 20, 0));
+            let closes = historical.map(company => company.close);
+            yData = closes.slice(Math.max(closes.length - 20, 0));
+            drawChart(xData, yData);
+        });
+
+}
+
+//Function to draw chart and update with fetched data
+function drawChart(xData, yData) {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: xData,
+            datasets: [{
+                label: "Stock Price History",
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(0, 0, 0)',
+                data: yData
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+}
+//first filter
+//function filterByMonth() {
+// grab the stockDays element
+// array.filter();
+// let currentDate = {
+//     year, 1970
+//     month, 07
+//     day 
+// }
+
+// currenDate.year = stockDays.historical[0]. //manipulate to grab the first 4 numbers anr parse
+// currenDate.month = stockDays.historical[0]. //manipulate to grab the first 4 numbers anr parse
+// currenDate.day = stockDays.historical[0]. //manipulate to grab the first 4 numbers anr parse
+
+// search for the element with the same year but that the month is +1. unless it is month 12. in that case year+1.
+// find method
+//filter
+
+//let date = '1990-07-02'
+//string manipulation skills / methods
+
+
+
+//}
+
+
+
+// for the last elements. // second filter
+
+
+//checkLast10Elements();
+//fetchNewUrl();
--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 
-
+//call function on click
 document.getElementById("button-addon2").addEventListener("click", GetValue);
+
+//define myDiv as global variable
 let myDiv = document.getElementById("results");
 
+//function to get value from input bar
+//start loader
+//Clean myDiv of previous results
+//call function value_search
 function GetValue() {
     let inputVal = document.getElementById("myInput").value;
     startLoader()
@@ -10,11 +16,15 @@ function GetValue() {
     value_search(inputVal)
 }
 
+//Builds the query url and calls function to fetch data
 function value_search(value) {
     let search = "https://financialmodelingprep.com/api/v3/search?query=" + value + "&limit=10&exchange=NASDAQ"
     fetch_search(search)
 }
 
+//Function to fetch data from url
+//Runs loop to create an additional element for each result of the search
+//Results are links to company.html file each with its own specific symbol
 function fetch_search(url) {
     fetch(url)
         .then(response => response.json())
@@ -22,7 +32,7 @@ function fetch_search(url) {
             for (let i = 0; i < data.length; i++) {
                 let line = document.createElement("a")
                 line.classList.add("listStyle");
-                line.href = "C:/ITC/project_two/company.html?symbol=" + data[i].symbol;
+                line.href = "company.html?symbol=" + data[i].symbol;
                 line.target = "_blank"
                 line.innerHTML += data[i].name + " (" + data[i].symbol + ")"
                 myDiv.appendChild(line)
