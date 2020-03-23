913e3974150c7d243b0ae167b9d0dc330024a797
Ghidon Fadlun <ghidon@hotmail.com>
2020-03-23 09:30:59 +0200

    Milestone 4 complete

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// get all stock price on page load
+document.addEventListener("DOMContentLoaded", marqueePrice);
+
+function marqueePrice() {
+  let searchTicker =
+    "https://financialmodelingprep.com/api/v3/stock/real-time-price";
+  fetch(searchTicker)
+    .then(response => response.json())
+    .then(data => {
+      prices = data.stockList;
+      let ticker = document.getElementById("ticker"); //element in html
+      prices.forEach(element => {
+        const symbol = element.symbol;
+        const price = element.price;
+        let symbolSpan = document.createElement("span");
+        symbolSpan.classList.add("blackText");
+        let priceSpan = document.createElement("span");
+        priceSpan.classList.add("green_light");
+        symbolSpan.innerHTML += " - " + symbol + " ";
+        priceSpan.innerHTML += "$" + price + " ";
+        ticker.appendChild(symbolSpan);
+        ticker.appendChild(priceSpan);
+      });
+    });
+}
 
 // event listeners
 document.getElementById("button-addon2").addEventListener("click", GetValue);
@@ -6,86 +31,75 @@ document.getElementById("button-addon2").addEventListener("click", GetValue);
 let myDiv = document.getElementById("results");
 
 function GetValue() {
-    let inputVal = document.getElementById("myInput").value;
-    startLoader()
-    myDiv.innerHTML = ""
-    searchValue(inputVal)
+  let inputVal = document.getElementById("myInput").value;
+  startLoader();
+  myDiv.innerHTML = "";
+  searchValue(inputVal);
 }
 
 //Builds the query url and calls function to fetch data
 async function searchValue(value) {
-    let search = "https://financialmodelingprep.com/api/v3/search?query=" + value + "&limit=10&exchange=NASDAQ"
-    const fetchedSearch = await fetchSearch(search);
-    fetchedSearch.map((company) => {
-
-        companyDetails = `https://financialmodelingprep.com/api/v3/company/profile/${company.symbol}`
-        fetch(companyDetails)
-            .then(response => response.json())
-            .then(data => {
-                company.image = data.profile.image
-                company.percentage = data.profile.changesPercentage
-                company.changes = data.profile.changes
-                let innerDiv = document.createElement("div")
-                innerDiv.classList.add("flexible");
-                let logo = document.createElement("img")
-                logo.src = company.image
-                logo.classList.add("logoSize");
-                let line = document.createElement("a")
-                innerDiv.classList.add("listStyle");
-                line.href = "company.html?symbol=" + company.symbol;
-                line.target = "_blank"
-                line.innerHTML += company.name
-                let symbol = document.createElement("span")
-                symbol.classList.add("symbolColor");
-                symbol.innerHTML = " (" + company.symbol + ")"
-                let changes = document.createElement("span")
-                changes.innerHTML += " " + company.changes
-                if (company.changes < 0) {
-                    changes.classList.add("green_light");
-                } else {
-                    changes.classList.add("red_light");
-                }
-                innerDiv.appendChild(logo)
-                innerDiv.appendChild(line)
-                line.appendChild(symbol)
-                line.appendChild(changes)
-                myDiv.appendChild(innerDiv)
-                stopLoader()
-
-            })
-
-    })
-    console.log(fetchedSearch)
+  let search =
+    "https://financialmodelingprep.com/api/v3/search?query=" +
+    value +
+    "&limit=10&exchange=NASDAQ";
+  const fetchedSearch = await fetchSearch(search);
+  fetchedSearch.map(company => {
+    companyDetails = `https://financialmodelingprep.com/api/v3/company/profile/${company.symbol}`;
+    fetch(companyDetails)
+      .then(response => response.json())
+      .then(data => {
+        company.image = data.profile.image;
+        company.percentage = data.profile.changesPercentage;
+        company.changes = data.profile.changes;
+        let innerDiv = document.createElement("div");
+        innerDiv.classList.add("flexible");
+        let logo = document.createElement("img");
+        logo.src = company.image;
+        logo.classList.add("logoSize");
+        let line = document.createElement("a");
+        innerDiv.classList.add("listStyle");
+        line.href = "company.html?symbol=" + company.symbol;
+        line.target = "_blank";
+        line.innerHTML += company.name;
+        let symbol = document.createElement("span");
+        symbol.classList.add("symbolColor");
+        symbol.innerHTML = " (" + company.symbol + ")";
+        let changes = document.createElement("span");
+        changes.innerHTML += " " + company.changes;
+        if (company.changes < 0) {
+          changes.classList.add("green_light");
+        } else {
+          changes.classList.add("red_light");
+        }
+        innerDiv.appendChild(logo);
+        innerDiv.appendChild(line);
+        line.appendChild(symbol);
+        line.appendChild(changes);
+        myDiv.appendChild(innerDiv);
+        stopLoader();
+      });
+  });
+  //console.log(fetchedSearch)
 }
 
+// Async function to fetch companies data
 async function fetchSearch(url) {
-    let companies = []
-    await fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            companies = data;
-            return companies
-        })
-    //.then(function (data) {
-    //   for (let i = 0; i < data.length; i++) {
-    //       let line = document.createElement("a")
-    //       line.classList.add("listStyle");
-    //       line.href = "company.html?symbol=" + data[i].symbol;
-    //       line.target = "_blank"
-    //       line.innerHTML += data[i].name + " (" + data[i].symbol + ")"
-    //       myDiv.appendChild(line)
-    //       c
-    //  }
-    //})
-    return companies
+  let companies = [];
+  await fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      companies = data;
+      return companies;
+    });
+  return companies;
 }
 
 //Stops Loader
 function stopLoader() {
-    document.getElementById("Loader").classList.add('loaded');
+  document.getElementById("Loader").classList.add("loaded");
 }
 //Starts Loader
 function startLoader() {
-    document.getElementById("Loader").classList.remove('loaded');
+  document.getElementById("Loader").classList.remove("loaded");
 }
-
