65e381b7fa4babeacdd28bc010420087ae523d74
Ghidon Fadlun <ghidon@hotmail.com>
2020-03-25 20:06:33 +0200

    OOP fixes. Functions to classes

--- Marquee.js
+++ Marquee.js
@@ -1,6 +1,7 @@
 class Marquee {
   constructor(element) {
     this.element = element;
+    this.marqueePrice();
   }
 
   marqueePrice() {
@@ -11,20 +12,23 @@ class Marquee {
       .then(data => {
         let prices = data.stockList;
         prices.forEach(element => {
-          const symbol = element.symbol;
-          const price = element.price;
-          let symbolSpan = document.createElement("span");
-          symbolSpan.classList.add("blackText");
-          let priceSpan = document.createElement("span");
-          priceSpan.classList.add("green_light");
-          symbolSpan.innerHTML += " - " + symbol + " ";
-          priceSpan.innerHTML += "$" + price + " ";
-          this.element.appendChild(symbolSpan);
-          this.element.appendChild(priceSpan);
+          this.createElement(element);
         });
       });
   }
+
+  createElement(element) {
+    const symbol = element.symbol;
+    const price = element.price;
+    let symbolSpan = document.createElement("span");
+    symbolSpan.classList.add("blackText");
+    let priceSpan = document.createElement("span");
+    priceSpan.classList.add("green_light");
+    symbolSpan.innerHTML += " - " + symbol + " ";
+    priceSpan.innerHTML += "$" + price + " ";
+    this.element.appendChild(symbolSpan);
+    this.element.appendChild(priceSpan);
+  }
 }
 
-let marqueeInstance = new Marquee(document.getElementById("ticker"));
-marqueeInstance.marqueePrice();
+new Marquee(document.getElementById("ticker"));
--- script.js
+++ script.js
@@ -70,39 +70,54 @@ async function searchValue(value) {
     companyDetails = `https://financialmodelingprep.com/api/v3/company/profile/${company.symbol}`;
     fetch(companyDetails)
       .then(response => response.json())
-      .then(data => {
-        company.image = data.profile.image;
-        company.percentage = data.profile.changesPercentage;
-        company.changes = data.profile.changes;
-        let innerDiv = document.createElement("div");
-        innerDiv.classList.add("flexible");
-        let logo = document.createElement("img");
-        logo.src = company.image;
-        logo.classList.add("logoSize");
-        let line = document.createElement("a");
-        innerDiv.classList.add("listStyle");
-        line.href = "company.html?symbol=" + company.symbol;
-        line.target = "_blank";
-        line.innerHTML += company.name;
-        let symbol = document.createElement("span");
-        symbol.classList.add("symbolColor");
-        symbol.innerHTML = " (" + company.symbol + ")";
-        let percentages = document.createElement("span");
-        percentages.innerHTML += " " + company.percentage;
-        if (company.changes < 0) {
-          percentages.classList.add("green_light");
-        } else {
-          percentages.classList.add("red_light");
-        }
-        innerDiv.appendChild(logo);
-        innerDiv.appendChild(line);
-        line.appendChild(symbol);
-        line.appendChild(percentages);
-        myDiv.appendChild(innerDiv);
-        stopLoader();
+      .then(newData => {
+        new CompanyListElement(company, newData, myDiv);
       });
+    stopLoader();
   });
-  //console.log(fetchedSearch)
+}
+
+class CompanyListElement {
+  constructor(company, additionalData, parent) {
+    this.company = company;
+    this.additionalData = additionalData;
+    this.parent = parent;
+    this.appendCompany();
+  }
+  appendCompany() {
+    let stockImage = this.additionalData.profile.image;
+    let stockPercentage = this.additionalData.profile.changesPercentage;
+    let stockChanges = this.additionalData.profile.changes;
+    let innerDiv = document.createElement("div");
+    innerDiv.classList.add("flexible");
+    let logo = document.createElement("img");
+    logo.src = stockImage;
+    logo.classList.add("logoSize");
+    let line = document.createElement("a");
+    innerDiv.classList.add("listStyle");
+    line.href = "company.html?symbol=" + this.company.symbol;
+    line.target = "_blank";
+    line.innerHTML += this.company.name;
+    let symbol = document.createElement("span");
+    symbol.classList.add("symbolColor");
+    symbol.innerHTML = " (" + this.company.symbol + ")";
+    let percentages = document.createElement("span");
+    percentages.innerHTML += " " + stockPercentage;
+    this.checkStockSign(stockChanges, percentages);
+    innerDiv.appendChild(logo);
+    innerDiv.appendChild(line);
+    line.appendChild(symbol);
+    line.appendChild(percentages);
+    this.parent.appendChild(innerDiv);
+  }
+
+  checkStockSign(stockChanges, element) {
+    return stockChanges < 0
+      ? element.classList.add("green_light")
+      : element.classList.add("red_light");
+  }
+
+  //remind question about loops
 }
 
 // Async function to fetch companies data
